521a8158734566b268bccd9a2c7c4ecdb0ba90ab
Mike Toronto <toronto@Torontos-MacBook-Air.local>
2020-03-29 19:48:57 -0500

    admin page

--- CAPSTONE_2/admin.php
+++ CAPSTONE_2/admin.php
@@ -1,8 +1,18 @@
-<?php include('./shared/navigation.php');?>
-<?php
+<?php include('./shared/navigation.php');
 session_start();
-
-
-
-
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+</head>
+
+<body>
+<h2 a href="#" class="">Create Admin Account</h2>
+<h2 a href="#" class="">Add Player to Database</h2>
+</body>
