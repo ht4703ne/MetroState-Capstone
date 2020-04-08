dcfef1a9c5f91e061dd31c10a8b624dff6e67e52
Dionce <36998509+Dionce@users.noreply.github.com>
2020-04-08 14:42:44 -0500

    Add files via upload
    These file are to delete from the players, we will have to update the SQL connection to match with the database.

---
+++ Deletepage.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+    <title>Delete Players </title>
+</head>
+    <body>
+        <table border =1 cellpadding=1 cellspacing=1>
+            <tr>
+                <th> Name </th> 
+                <th> Delete </th>
+            </tr>
+            <?php
+                //connect
+                $con = mysqli_connect('localhost', 'root', 'admin');
+
+                    mysqli_select_db($con, 'ffb_stats');
+
+                $sql = "SELECT * FROM player";
+
+                //Execute the query
+                $records = mysqli_query($con, $sql);
+
+                while($row = mysqli_fetch_array($records))
+                {
+                    echo "<tr>";
+                    echo "<td>" .$row['NAME'] . "</td>";
+                    echo "<td><a href=delete.php?id=" .$row['ID_PK'] .">Delete</a></td>";
+                }
+
+            ?>
+        </table>
+
+    </body>
+
+</html>
